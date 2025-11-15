9981201d109cdf7f592981b7cc9f7dbf2120b07f
ANurag Yadav <anurag.yadav8949@yahoo.com>
2025-11-15 14:45:45 +0530

    Improve bubbleSort by limiting inner loop range
    Optimize the inner loop to reduce unnecessary comparisons.

--- 09_sorting/bubbleSort.js
+++ 09_sorting/bubbleSort.js
@@ -3,7 +3,7 @@ const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 function bubbleSort(array) {
   const length = array.length;
   for (let i = 0; i < length; i++) {
-    for (let j = 0; j < length; j++) { 
+    for (let j = 0; j < length - i - 1 ; j++) { 
       if(array[j] > array[j+1]) {
         //Swap the numbers
         let temp = array[j]
